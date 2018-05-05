7d0b6cdcb3ea8aee944cbc7038e1e81e592ffae3
abing <binglianghuang@gmail.com>
2018-05-05 23:56:33 +0800

    bat

---
+++ v3/run-update.bat
@@ -0,0 +1,14 @@
+@echo off  
+Y:  
+cd Y:\gitrepo\goldminer\v3
+start python IndexBar.py
+
+start python IndexConstituents.py
+
+start python StockTradingDerivativeIndicator.py
+
+start python StockPrimaryFinanceIndicator.py
+
+start python StockIncomeStatement.py
+
+start python StockBalanceSheet.py
