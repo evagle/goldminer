7d38a7b38f5164a715e66b5982cc602c6964f898
Bingliang <blhuang@amazon.com>
2018-05-15 15:56:48 +0800

    pepb generator

--- evaluation/IndexPEPBGenerator.py
+++ evaluation/IndexPEPBGenerator.py
@@ -3,12 +3,15 @@ from datetime import datetime, timedelta, date
 
 from evaluation.IndexConstituentManager import IndexConstituentManager
 from evaluation.StockManager import StockManager
+from models.models import IndexPrimaryIndicator
+from storage.IndexPrimaryIndicatorDao import IndexPrimaryIndicatorDao
 
 
 class IndexPEPBGenerator:
     def __init__(self):
         self.indexConstituent = IndexConstituentManager()
         self.stockManager = StockManager()
+        self.indexPrimaryIndicatorDao = IndexPrimaryIndicatorDao()
 
     def getProfitAt(self, indexCode, date):
         sql = "SELECT code, pub_date, end_date, NETPROFIT FROM income_statement WHERE code = '%s' and pub_date <= '%s' ORDER BY pub_date LIMIT 1"
@@ -27,29 +30,33 @@ class IndexPEPBGenerator:
         else:
             return result[0][0]
 
-    def equalWeightedPE(self, indexCode, startDate = None):
+    def EqualWeightedPE(self, indexCode, startDate = None):
         d = date(2005, 1, 4) if startDate is None else startDate
         now = datetime.now().date()
-        equalWeightPE = {}
+
+        models = []
         print(d, now)
         while d <= now:
             if self.stockManager.isTradeDate(d):
+                model = IndexPrimaryIndicator()
+                model.code = indexCode
                 constituents = self.indexConstituent.getConstituents(indexCode, d)
                 if constituents is not None:
                     stockPETTM = [self.stockManager.getStockPETTM(stock, d) for stock in constituents]
                     pe = len(stockPETTM) / sum([1 / p if p > 0 else 0 for p in stockPETTM])
-                    print(date, pe)
-                    equalWeightPE[d] = pe
+                    model.trade_date = d
+                    model.equal_weight_pe = pe
+                    models.append(model)
                 else:
                     print("error", indexCode, d)
             d = d + timedelta(days=1)
-        return equalWeightPE
 
-    def updateAllByIndex(self, indexCode):
-        peManager.equalWeightedPE("000913")
+        return models
+
 
 if __name__ == "__main__":
     peManager = IndexPEPBGenerator()
-    print(peManager.equalWeightedPE("000913"))
+    models = peManager.calcEqualWeightedPE("000913")
+
 
 
--- storage/IndexPrimaryIndicatorDao.py
+++ storage/IndexPrimaryIndicatorDao.py
@@ -1,19 +1,21 @@
 # coding: utf-8
 from datetime import date
 
-from models.models import IndexDailyBar, IndexPrimaryIndicator
+from models.models import IndexPrimaryIndicator
 from storage.BaseDao import BaseDao
+from sqlalchemy.sql import column
 
 
 class IndexPrimaryIndicatorDao(BaseDao):
 
-    def add(self, indicator: IndexPrimaryIndicator):
-        self.session.add(indicator)
-        self.session.commit()
-
-    def getLatestDate(self, code: str):
+    def getLatestDate(self, code: str, columnName):
         result = self.session.query(IndexPrimaryIndicator.trade_date)\
                              .filter(IndexPrimaryIndicator.code == code)\
+                             .filter_by(column(columnName).isnot(None))\
                              .order_by(IndexPrimaryIndicator.trade_date.desc())\
                              .first()
-        return date(2001, 1, 1) if result is None else result[0]
+
+        return date(2005, 1, 4) if result is None else result[0]
+
+    def batchUpdate(self, models):
+        self.session.bulk_save_objects(models)
