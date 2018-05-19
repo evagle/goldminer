dd7d26a3675c6a80443696cc8c885b7bd00c74a4
abing <binglianghuang@gmail.com>
2018-05-19 11:37:44 +0800

    stub for stock spider, now have problem download stock list from Shanghai exchange

---
+++ spider/v3/StockSpider.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import json
+
+import tushare as ts
+import pandas as pd
+'''
+从上交所和深交所下载最新股票列表，更新stock表
+'''
+from common.HttpUtil import HttpUtil
+
+
+class StockSpider:
+
+    def getStockAFromSH(self):
+        pass
+
+spider = StockSpider()
+spider.getStockAFromSH()
