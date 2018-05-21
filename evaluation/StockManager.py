cc89d9a76062a3145efe994fd2e16ea27252d04d
abing <binglianghuang@gmail.com>
2018-05-21 22:01:49 +0800

    getSuspensionDates of stock

--- evaluation/StockManager.py
+++ evaluation/StockManager.py
@@ -1,5 +1,8 @@
 # coding: utf-8
-from models.models import TradingDerivativeIndicator, IndexDailyBar
+from models.models import TradingDerivativeIndicator, IndexDailyBar, StockDailyBarAdjustNone
+from storage.IndexesDao import IndexesDao
+from storage.StockDailyBarAdjustNoneDao import StockDailyBarAdjustNoneDao
+from storage.StockDao import StockDao
 from storage.StockFundamentalsDao import StockFundamentalsDao
 from datetime import date
 
@@ -8,11 +11,14 @@ class StockManager:
 
     def __init__(self):
         self.fundamentalsDao = StockFundamentalsDao()
+        self.stockDao = StockDao()
+
         self.__stockPETTMCache = {}
         self.__tradeDatesCache = []
 
         # load trade dates when initiate
         self.__loadTradeDates()
+        self.stockPETTMNotFound = {}
 
     def __loadStockPETTM(self, stockCode):
         if stockCode in self.__stockPETTMCache:
@@ -26,12 +32,21 @@ class StockManager:
         for row in result:
             pe[row[0].strftime("%Y-%m-%d")] = row[1]
         self.__stockPETTMCache[stockCode] = pe
-        print("Load stock %s PETTM successfully" % stockCode)
+        print("[%s] Load %d PETTM successfully" % (stockCode, len(pe)))
 
     def getStockPETTM(self, stockCode, d: date):
         if stockCode not in self.__stockPETTMCache:
             self.__loadStockPETTM(stockCode)
-        return self.__stockPETTMCache[stockCode][d.strftime("%Y-%m-%d")]
+
+        datestr = d.strftime("%Y-%m-%d")
+        if datestr in self.__stockPETTMCache[stockCode]:
+            return self.__stockPETTMCache[stockCode][datestr]
+        else:
+            if stockCode in self.stockPETTMNotFound:
+                self.stockPETTMNotFound[stockCode] += 1
+            else:
+                self.stockPETTMNotFound[stockCode] = 1
+            return 0
 
     def __loadTradeDates(self):
         session = self.fundamentalsDao.getSession()
@@ -45,4 +60,15 @@ class StockManager:
 
     def isTradeDate(self, d: date):
         return d in self.__tradeDatesCache
+
+    def getSuspensionDates(self, code):
+        stockBarDao = StockDailyBarAdjustNoneDao()
+        tradeDates = stockBarDao.getAllTradeDatesByCode(code)
+        candidates = list(set(self.__tradeDatesCache).difference(set(tradeDates)))
+        startDate = self.stockDao.getStockPublishDate(code)
+        suspensionDates = [d for d in candidates if d >= startDate]
+        suspensionDates.sort()
+        print("[%s] %d suspension dates" % (code,  len(suspensionDates)))
+        return suspensionDates
+
 
