cd468a340df143d4e3ea51dea32e478f95cf9c69
abing <binglianghuang@gmail.com>
2018-05-31 08:10:47 +0800

    getWeight adjust because of table split

--- evaluation/IndexConstituentManager.py
+++ evaluation/IndexConstituentManager.py
@@ -6,17 +6,20 @@ from storage.IndexConstituentDao import IndexConstituentDao
 
 import json
 
+from storage.IndexWeightDao import IndexWeightDao
+
 
 class IndexConstituentManager:
     def __init__(self):
         self.indexConstituentDao = IndexConstituentDao()
+        self.indexWeightDao = IndexWeightDao()
 
     def getWeights(self, code, date):
-        result = self.indexConstituentDao.getConstituents(code, date)
-        if result is None:
+        model = self.indexWeightDao.getConstituents(code, date)
+        if model is None:
             return None
 
-        data = json.loads(result[2])
+        data = json.loads(model.constituents)
         weights = {}
         for k in data:
             weights[k[5:]] = data[k]
@@ -24,18 +27,14 @@ class IndexConstituentManager:
         return weights
 
     def __formatConstituents(self, model: IndexConstituent):
-        if model.no_weight == 1:
-            constituents = [c[5:] for c in json.loads(model.constituents)]
-        else:
-            keys = list(json.loads(model.constituents).keys())
-            constituents = [c[5:] for c in keys]
+        constituents = json.loads(model.constituents)
         return [model.code, model.trade_date, constituents]
 
     # 获取指数成份股，如果能获取到weights则直接返回
     # 否则尝试获取constituents
     # trade_date >= query_date && trade_date 最小
     def _getConstituents(self, code, date):
-        result = self.indexConstituentDao.getConstituents_1(code, date)
+        result = self.indexConstituentDao.getConstituents(code, date)
         if result is not None:
             return self.__formatConstituents(result)
         return None
@@ -57,3 +56,7 @@ class IndexConstituentManager:
         for c in data:
             constituents.append(c[5:])
         return constituents
+
+if __name__ == "__main__":
+    manager = IndexConstituentManager()
+    manager.getWeights('000001', date(2017, 1, 4))
