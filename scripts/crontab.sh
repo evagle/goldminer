009d04881cfe30638e983f112d352ab32dceb19d
abing <binglianghuang@gmail.com>
2018-06-27 07:58:04 +0800

    bash not python

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-3 0 * * * /usr/local/anaconda3/bin/python /home/abing/goldminer/scripts/run-service.sh > /home/abing/goldminer/log/gm.log 2>&1
+3 0 * * * /bin/bash /home/abing/goldminer/scripts/run-service.sh > /home/abing/goldminer/log/gm.log 2>&1
