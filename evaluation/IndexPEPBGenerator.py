6874bda7389658a5b1b93d402789e31ec522a23d
Bingliang <blhuang@amazon.com>
2018-06-29 18:07:23 +0800

    IndexWeightedPEProcessor

--- evaluation/IndexPEPBGenerator.py
+++ evaluation/IndexPEPBGenerator.py
@@ -1,7 +1,5 @@
 # coding: utf-8
-from datetime import datetime, timedelta, date
 
-from evaluation.IndexConstituentManager import IndexConstituentManager
 from evaluation.IndexEqualWeightPBProcessor import IndexEqualWeightPBProcessor
 from evaluation.IndexEqualWeightPEProcessor import IndexEqualWeightPEProcessor
 from evaluation.IndexMaxPbProcessor import IndexMaxPBProcessor
@@ -13,12 +11,8 @@ from evaluation.IndexPBGradeTenYearProcessor import IndexPBGradeTenYearProcessor
 from evaluation.IndexPBHeightTenYearProcessor import IndexPBHeightTenYearProcessor
 from evaluation.IndexPEGradeTenYearProcessor import IndexPEGradeTenYearProcessor
 from evaluation.IndexPEHeightTenYearProcessor import IndexPEHeightTenYearProcessor
-from evaluation.StockManager import StockManager
-from models.models import IndexPrimaryIndicator
-from storage.IndexConstituentDao import IndexConstituentDao
-from storage.IndexPrimaryIndicatorDao import IndexPrimaryIndicatorDao
+from evaluation.IndexWeightedPEProcessor import IndexWeightedPEProcessor
 from storage.IndexesDao import IndexesDao
-from storage.StockDao import StockDao
 
 
 class IndexPEPBGenerator:
@@ -26,8 +20,10 @@ class IndexPEPBGenerator:
         self.indexDao = IndexesDao()
 
         self.equalWeightPEProcessor = IndexEqualWeightPEProcessor()
+        self.weightedPEProcessor = IndexWeightedPEProcessor()
         self.medianPEProcessor = IndexMedianPEProcessor()
         self.equalWeightPBProcessor = IndexEqualWeightPBProcessor()
+
         self.medianPBProcessor = IndexMedianPBProcessor()
         self.maxPEProcessor = IndexMaxPEProcessor()
         self.maxPBProcessor = IndexMaxPBProcessor()
@@ -41,6 +37,7 @@ class IndexPEPBGenerator:
         indexes = self.indexDao.getIndexList()
         for code in indexes:
             self.equalWeightPEProcessor.process(code)
+            self.weightedPEProcessor.process(code)
             self.medianPEProcessor.process(code)
             self.equalWeightPBProcessor.process(code)
             self.medianPBProcessor.process(code)
@@ -51,6 +48,7 @@ class IndexPEPBGenerator:
             self.pbHeightProcessor.process(code)
             self.peGradeProcessor.process(code)
             self.pbGradeProcessor.process(code)
+
 
 if __name__ == "__main__":
     peManager = IndexPEPBGenerator()
