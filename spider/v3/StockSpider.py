335e429dd1fffee8ebe15e29ed6b92d06fa8d8d2
abing <binglianghuang@gmail.com>
2018-07-22 22:56:02 +0800

    从上交所下载股票列表并导入

--- spider/v3/StockSpider.py
+++ spider/v3/StockSpider.py
@@ -15,6 +15,9 @@ class StockSpider():
         super(StockSpider, self).__init__()
         self.stockDao = StockDao()
 
+    '''
+    从tushare下载所有股票列表，更新stock数据库
+    '''
     def getStockFromTuShare(self):
         data = ts.get_stock_basics()
         for code, row in data.iterrows():
@@ -36,6 +39,29 @@ class StockSpider():
                 stock.circulation_stock = row.outstanding*1e8
                 self.stockDao.add(stock)
                 print(stock)
+
+    '''
+    从上交所下载股票列表然后导入
+    下载地址
+    http://www.sse.com.cn/assortment/stock/list/share/
+    '''
+    def importFromSH(self):
+        dao = StockDao()
+        for line in open("/Users/abing/Downloads/ee.txt").readlines():
+            args = line.split()
+
+            old = dao.getByCode(args[0])
+            if old is None:
+                stock = Stock()
+                stock.exchange = 1
+                stock.code = args[0]
+                stock.name = args[1]
+                stock.pub_date = args[4]
+                stock.total_stock = args[5]
+                stock.circulation_stock = args[6]
+                stock.industry = ''
+                dao.add(stock)
+                print(args)
 
 
 if __name__ == "__main__":
