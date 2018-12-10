6cd8ceaeba067ed3b60215ae0700882d195dea8d
abing <binglianghuang@gmail.com>
2018-12-10 08:42:50 +0800

    kline buy point investigation

--- investigation/buy_point_investigation.py
+++ investigation/buy_point_investigation.py
@@ -15,8 +15,17 @@ import numpy as np
 import pandas as pd
 
 
+class SellStrategies:
+    # 卖出策略1：
+    #    1. 7%止损线
+    #    2. 盈利15%后，止损线提高到5%
+    #    3. 之后跌破50日线或者跌到20%止损
 
-class FallbackNewHigh:
+    def sellStrategy1(self, buyPrice, bars):
+        pass
+
+
+class BuyPointInvestigation:
     def __init__(self):
         self.stockBarDao = StockDailyBarAdjustPrevDao()
         self.stockFundamentals = StockFundamentalsDao()
@@ -164,16 +173,19 @@ class FallbackNewHigh:
                 break
         return (bars[pos].close - lowest_bar.close) / lowest_bar.close if lowest_bar else 0
 
-    # 策略1 上涨后长期整理，然后突破形态
-    def fallback_new_high_buy_points(self, code):
+
+    def strategy1(self, code):
         bars = self.stockBarDao.getAll(code)
         derivatives = self.stockFundamentals.getAll(code, TradingDerivativeIndicator)
-        primary_finance_indicators = self.stockFundamentals.getAll(code, PrimaryFinanceIndicator)
-        income_statements = self.stockFundamentals.getAll(code, IncomeStatement)
+        # if len(bars) != len(derivatives):
+            # print(len(bars), len(derivatives))
+            # print("!!![ERROR] bars != derivatives")
+            # return
 
         n = len(bars)
 
         closes = np.array([bar.close for bar in bars])
+        print(np.mean(closes))
         if np.isnan(np.mean(closes)):
             return (-1, -1)
 
@@ -197,13 +209,8 @@ class FallbackNewHigh:
         for i in range(n):
             bars[i].sma250 = sma250[i]
 
-        for bar in bars:
-            bar.outer_amplitude = (bar.high - bar.low)/bar.low if bar.low > 0 else 0
-
-        for bar in bars:
-            bar.inner_amplitude = math.fabs(bar.close - bar.open)/bar.open if bar.open > 0 else 0
-
         # 记录之前出现过的买点，不重复计算
+        # 如果出现包含的买点，例如(a,b), (c,d), a<c<d<b, 则应该按照c,d计算
         win = 0
         total = 0
         win_ratio = []
@@ -211,7 +218,7 @@ class FallbackNewHigh:
 
         buy_points = []
         for i in range(250, n):
-            # 左边7个K线小于当前点，右边15个K线小于当前点
+            # 左边10个K线小于当前点，右边30个K线小于当前点
             left = right = True
             for j in range(i - 7, i):
                 if bars[j].high > bars[i].high:
@@ -236,90 +243,39 @@ class FallbackNewHigh:
                              bars[j].close > bars[j].sma250
 
                     # 调整周期长度小于250天
-                    adjust_period = j - i
-                    period_lt_250 = (adjust_period <= 250)
+                    period_lt_250 = (j - i <= 250)
 
-                    # 算PE，PB高度
-                    pe_height = self.calculate_pe_heigt_eight_year(bars[j].trade_date, derivatives)
-                    pb_height = self.calculate_pb_heigt_eight_year(bars[j].trade_date, derivatives)
+                    # 算PE，PB高度 < 70%
+                    pe_height = self.calcPEHeight(bars[j].trade_date, derivatives)
 
                     # 平均换手率 < 3% (or avg)
-                    average_turn_rate = self.calculate_average_turn_rate(bars[i].trade_date, bars[j].trade_date, derivatives)
 
                     # 调整期内的方差
-                    variance = self.calculate_variance(bars[i:j])
-
-                    # 到j日已经几日新高
-                    days_with_lower_price = self.calculate_days_with_lower_price(bars, j)
-
-                    # 到j日已经几日新高
-                    new_high_days = self.calculate_new_high_days(bars, j)
-
-                    # 最低点算起到j日的天数
-                    days_from_bottom = self.calculate_days_from_bottom(bars, j)
-
-                    # 均线关系
-                    sma50_120 = bars[j].sma50 / bars[j].sma120
-                    sma120_250 = bars[j].sma120 / bars[j].sma250
-                    sma10_120 = bars[j].sma10 / bars[j].sma120
-                    sma10_250 = bars[j].sma10 / bars[j].sma250
-
-                    # close/sma250
-                    close_sma250 = bars[j].close / bars[j].sma250
-                    close_sma50 = bars[j].close / bars[j].sma50
+                    var = self.calculate_variance(bars[i:j])
 
                     # 长期缓慢上涨股还是阴跌暴涨股，统计上涨时间/下跌时间
 
                     # 历史策略成功率
 
-                    # 在i点之前已经上升的幅度，即此次调整前上升了多少,
-                    # 找最低点方法，最低点距离左侧跌了25%，右侧上升到i点
-                    increase_before_fallback = self.calculate_increase_before_fallback(bars, i)
+                    # 在i点之前已经上升的幅度，即此次调整前上升了多少
 
                     # j点的RPS强度
 
                     # j点的交易量/(i,j)的平均交易量
-                    volume_ratio = self.calculate_volume_ratio(bars, i, j)
 
-                    # 同比净利润增速
-                    profit_growth = self.calculate_quarter_profit_growth(income_statements, bars[j].trade_date) * 100
+                    # 当期和上年度的净利润增速
 
-                    # 同比营收增速
-                    business_growth = self.calculate_quarter_business_growth(income_statements, bars[j].trade_date) * 100
-
-                    # 同比EPS增速
-                    eps_growth = self.calculate_eps_growth(primary_finance_indicators, bars[j].trade_date) * 100
-
-                    # i,j之间平均日内振幅，振幅=(high-low)/low
-                    average_outer_amplitude = np.mean([bar.outer_amplitude for bar in bars[i:j]]) * 100
-
-                    # i,j之间平均日内波动，波动=abs(close-open)/open
-                    average_inner_amplitude = np.mean([bar.inner_amplitude for bar in bars[i:j]]) * 100
-
-                    # 10日日内振幅用来作为卖出指标判断
-
-                    finance_indicator = self.get_primary_finance_indicator_by_date(bars[j].trade_date, primary_finance_indicators)
-                    eps_basic = finance_indicator.EPSBASIC
-
-                    roe = finance_indicator.ROEWEIGHTED
-
-                    #TODO 是否有跳空缺口
-                    # up_break_through_gap =
-
-                    #TODO RPS股价强弱指标
-                    #rps =
-
-                    #TODO eps_rs, eps relative strength, 类似rps计算方式
-                    #eps_rs
-
-                    # print("**debug", code, bars[j].trade_date, pe_height, pb_height, average_turn_rate, variance, new_high_days, \
-                    #       sma50_120, sma120_250, sma10_20, sma10_250, volume_ratio, profit_growth)
 
                     if max_decrease_percent < 30 and \
-                            gt_MAs and \
-                            bars[j].trade_date not in seen_dates:
+                            gt_MAs and period_lt_250 and \
+                            bars[j].sma120 > bars[j].sma250 * 0.96 and \
+                            bars[j].sma50 > bars[j].sma120 * 0.99 and \
+                            var <= 0.5 and \
+                            bars[j].trade_date not in seen_dates and \
+                            bars[j].trade_date > datetime(2016,1,1).date():
 
                         seen_dates.append(bars[j].trade_date)
+
                         # 计算止损线为7%，能获得的最大收益
                         max_price = 0
                         days_to_max_price = 0
@@ -328,91 +284,99 @@ class FallbackNewHigh:
                             if bars[k].close < bars[j].close * 0.93:
                                 break
 
+                            days_to_max_price = k - j
                             if bars[k].close > max_price:
                                 max_price = bars[k].close
                                 max_price_date = bars[k].trade_date
-                                days_to_max_price = k - j
-
-
-                        gain = (max_price - bars[j].close) * 100.0 / bars[j].close
-                        target = (gain >= 50)
-
-
-                        derivative = self.get_derivatives_by_date(bars[j].trade_date, derivatives)
-
-                        buy_point = {}
-                        buy_point["code"] = code
-                        buy_point["start_date"] = bars[i].trade_date
-                        buy_point["end_date"] = bars[j].trade_date
-                        buy_point["max_decrease"] = max_decrease_percent
-                        buy_point["pe_height"] = pe_height
-                        buy_point["pb_height"] = pb_height
-                        buy_point["average_turn_rate"] = average_turn_rate
-                        buy_point["variance"] = variance
-
-                        buy_point["average_outer_amplitude"] = average_outer_amplitude
-                        buy_point["average_inner_amplitude"] = average_inner_amplitude
-                        buy_point["volume_ratio"] = volume_ratio
-                        buy_point["profit_growth"] = profit_growth
-                        buy_point["business_growth"] = business_growth
-                        buy_point["eps_growth"] = eps_growth
-
-                        buy_point["increase_before_fallback"] = increase_before_fallback
-                        buy_point["pe"] = derivative.PETTM
-                        buy_point["pb"] = derivative.PB
-                        buy_point["mkt_cap"] = int(derivative.TOTMKTCAP/decimal.Decimal(1e8))
-                        buy_point["adjust_period"] = adjust_period
-                        buy_point["sma50_120"] = sma50_120
-                        buy_point["sma120_250"] = sma120_250
-                        buy_point["sma10_120"] = sma10_120
-                        buy_point["sma10_250"] = sma10_250
-                        buy_point["close_sma50"] = close_sma50
-                        buy_point["close_sma250"] = close_sma250
-
-                        buy_point["new_high_days"] = new_high_days
-                        buy_point["days_with_lower_price"] = days_with_lower_price
-                        buy_point["days_from_bottom"] = days_from_bottom
-
-                        buy_point["roe"] = roe
-                        buy_point["eps"] = eps_basic
-
-                        buy_point["gain"] = gain
-                        buy_point["target"] = 1 if target else 0
-                        buy_points.append(buy_point)
+                            # 考虑止盈策略时的收益
+                            # if bars[k].close < max(max_price*0.9, bars[j].close*0.93):
+                            #     if bars[k].close > bars[j].close * 1.2:
+                            #         win+=1
+                            #         print("win++")
+                            #     max_price = bars[k].close
+                            #     break
+                            # end止盈策略
+                        p = (max_price - bars[j].close) * 100.0 / bars[j].close
+                        buy_points.append({
+                            "code": code,
+                            "start": bars[i],
+                            "end": bars[j],
+                            "gain": p,
+                            "days": days_to_max_price,
+                            "var": var,
+                            "max_price_date": max_price_date
+                        })
                     break
 
-        return pd.DataFrame.from_records(buy_points)
+        # 删除相互包含的买点，保留第一个买点，即包含的
+        for i in range(1, len(buy_points)):
+            if buy_points[i]["end"].trade_date <= buy_points[i-1]["end"].trade_date:
+                buy_points[i - 1]["remove"] = 1
 
+        buy_points_cleaned = []
+        for i in range(0, len(buy_points)):
+            buy_point = buy_points[i]
+            if "remove" not in buy_point:
+                buy_points_cleaned.append(buy_point)
+
+        buy_points = buy_points_cleaned
+
+        # 删除max_price_date相等的买点，保留第一个买点
+        for i in range(1, len(buy_points)):
+            if buy_points[i]["max_price_date"] == buy_points[i-1]["max_price_date"]:
+                buy_points[i]["remove"] = 1
+
+        buy_points_cleaned = []
+        for i in range(0, len(buy_points)):
+            buy_point = buy_points[i]
+            if "remove" not in buy_point:
+                buy_points_cleaned.append(buy_point)
+
+        buy_points = buy_points_cleaned
+
+        for buy_point in buy_points:
+            bar_start = buy_point["start"]
+            bar_end = buy_point["end"]
+            print(bar_start.code, bar_start.trade_date, bar_start.close, bar_end.trade_date, bar_end.close,
+                  str(buy_point["gain"]) + "%", buy_point["days"], "d=", buy_point["max_price_date"])
+            if buy_point["gain"] > 50:
+                win+=1
+                win_ratio.append(buy_point["gain"])
+            total+=1
+        print(code, "total = ", total, "win = ", win, "loss = ", total - win, "win_ratio", np.mean(win_ratio),
+              win_ratio)
+        return total, win
 
 
 if __name__ == "__main__":
-    analyzer = FallbackNewHigh()
+    analyzer = BuyPointInvestigation()
     stockDao = StockDao()
     stocks = stockDao.getStockList()
 
-    df = analyzer.fallback_new_high_buy_points("601318")
+    df = analyzer.strategy1("601318")
     df.to_csv("~/buypoints.tsv", sep="\t", index=False)
+    # analyzer.strategy1('000001')
+    # analyzer.strategy1('000739')
+    # analyzer.strategy1('600149')
+    #
+    # analyzer.strategy1('600466')
+    # analyzer.strategy1('601390')
+    # analyzer.strategy1('600028')
 
 
-    random.shuffle(stocks)
-    training_data = None
-    num = 2000
-    for code in stocks[:num]:
-        print(code)
-        try:
-            df = analyzer.fallback_new_high_buy_points(code)
-            if type(df) == tuple:
-                print("Data error ", code)
-                continue
-        except Exception:
-            print("Data error ", code)
-            continue
-
-        if training_data is None:
-            training_data = df
-        else:
-            training_data = pd.concat([training_data, df])
-
-    output_filename = "fallback_new_high_%d_%s.tsv" % (num, datetime.now().strftime("%Y-%m-%d"))
-    training_data.to_csv(output_filename, sep="\t", index=False)
+    # random.shuffle(stocks)
+    # total = 0
+    # win = 0
+    # n = 0
+    # for code in stocks:
+    #     (t,w) = analyzer.strategy1(code)
+    #     if t == -1:
+    #         continue
+    #     total += t
+    #     win += w
+    #     n+=1
+    #     if n > 50:
+    #         break
+    #
+    # print(total, win)
 
