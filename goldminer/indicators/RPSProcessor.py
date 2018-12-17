f761b26f49324fd3fc0f8d78c900c963810bbc12
abing <binglianghuang@gmail.com>
2018-12-17 22:48:05 +0800

    rps indicator generator

--- goldminer/indicators/RPSProcessor.py
+++ goldminer/indicators/RPSProcessor.py
@@ -9,8 +9,24 @@ class RPSProcessor:
 
     def process(self, trade_date):
         bars = self.stockBarPrevDao.getByDate(trade_date)
+        # 过滤掉B股
+        filtered = []
+        for bar in bars:
+            if not bar.code.startswith("9"):
+                filtered.append(bar)
+        bars = filtered
+
         total_count = len(bars)
         MIN = -1e6
+
+        has_rps = True
+        for bar in bars:
+            if bar.rps50 is None or bar.rps50 is None or bar.rps50 is None:
+                has_rps = False
+
+        if has_rps:
+            print("rps already calculated for trade_date", trade_date)
+            return None
 
         bars50 = sorted(bars, key=lambda bar: bar.gain50 if bar.gain50 is not None else MIN, reverse=True)
         for i in range(total_count):
@@ -18,7 +34,7 @@ class RPSProcessor:
             if bar.gain50 is None:
                 bar.rps50 = 0
             else:
-                bar.rps50 = 100 - (i+1)*100/total_count
+                bar.rps50 = 100 - (i + 1) * 100 / total_count
 
         bars120 = sorted(bars, key=lambda bar: bar.gain120 if bar.gain120 is not None else MIN, reverse=True)
         for i in range(total_count):
@@ -36,6 +52,7 @@ class RPSProcessor:
             else:
                 bar.rps250 = 100 - (i + 1) * 100 / total_count
 
+        print(len(bars), "updated")
         self.stockBarPrevDao.bulkSave(bars)
 
 
@@ -47,4 +64,3 @@ if __name__ == "__main__":
         print("start", d)
         processor.process(d)
         print("end", d)
-        
