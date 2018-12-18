29455598b6ca30c1f5d5cc14adcc918117a431fa
abing <binglianghuang@gmail.com>
2018-12-18 08:31:00 +0800

    scripts rename

--- scripts/crontab.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-3 0 * * * /bin/bash /home/abing/goldminer/scripts/run-service.sh > /home/abing/goldminer/log/gm.log 2>&1
---
+++ scripts/goldminer_crontab.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+3 0 * * * /bin/bash goldminer_daily_run.sh > /home/abing/goldminer/log/gm.log 2>&1
