7f38c632a616d093a237fe952f793abdc0eed8fd
abing <binglianghuang@gmail.com>
2018-12-18 08:27:28 +0800

    run service update, scripts are installed in python/bin

--- scripts/run-service.sh
+++ scripts/run-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/anaconda3/bin/python /home/abing/goldminer/SpidersConstituents.py > /home/abing/goldminer/log/constituents.log 2>&1
-/usr/local/anaconda3/bin/python /home/abing/goldminer/Spiders.py > /home/abing/goldminer/log/spiders.log 2>&1
-/usr/local/anaconda3/bin/python /home/abing/goldminer/Evaluations.py > /home/abing/goldminer/log/evaluations.log 2>&1
-/usr/local/anaconda3/bin/python /home/abing/goldminer/Funds.py > /home/abing/goldminer/log/funds.log 2>&1
+goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
+goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
+goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
+goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
