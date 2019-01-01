190779bcec5a93732919012a1a0490305d63c7f4
abing <binglianghuang@gmail.com>
2019-01-01 22:30:30 +0800

    daily run script use absolute path

--- scripts/goldminer_daily_run.sh
+++ scripts/goldminer_daily_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
-goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
-goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
-goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
+path = "/usr/local/anaconda3/bin/"
+
+python $path/goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
+python $path/goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
+python $path/goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
+python $path/goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
