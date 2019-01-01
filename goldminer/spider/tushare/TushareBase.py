f32e8091c6e411329db40e0fd2fa3101d1094032
abing <binglianghuang@gmail.com>
2019-01-01 19:43:02 +0800

    tushare api

--- goldminer/spider/tushare/TushareBase.py
+++ goldminer/spider/tushare/TushareBase.py
@@ -1,3 +1,15 @@
 # coding: utf-8
 
+import tushare as ts
 
+
+class TushareBase:
+    def __init__(self):
+        self.ts_pro_api = ts.pro_api('707e43764dbbf6de8eb6a077ee68fa977b4190c014d5ce3ce74e3dc7')
+
+    def to_ts_code(self, code):
+        if code[0:1] == "6":
+            ts_code = code + ".SH"
+        else:
+            ts_code = code + ".SZ"
+        return ts_code
