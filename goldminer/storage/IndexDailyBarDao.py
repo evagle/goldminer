906dbe1bf1e0678f5325d097427c771ab8a3f635
abing <binglianghuang@gmail.com>
2019-01-24 22:34:53 +0800

    getBatchIndexBar

--- goldminer/storage/IndexDailyBarDao.py
+++ goldminer/storage/IndexDailyBarDao.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from datetime import date
+from typing import List
 
 from goldminer.models.models import IndexDailyBar
 from goldminer.storage.BaseDao import BaseDao
@@ -16,6 +17,18 @@ class IndexDailyBarDao(BaseDao):
             .filter(IndexDailyBar.code == code) \
             .filter(IndexDailyBar.trade_date == d) \
             .first()
+
+    def getBatchByDate(self, codes: list, d: date) -> List[IndexDailyBar]:
+        if codes is None:
+            return []
+        bars = self.session.query(IndexDailyBar) \
+            .filter(IndexDailyBar.trade_date == d) \
+            .all()
+        result = []
+        for bar in bars:
+            if bar.code in codes:
+                result.append(bar)
+        return result
 
     def getLatestDate(self, code):
         result = self.session.query(IndexDailyBar.trade_date)\
