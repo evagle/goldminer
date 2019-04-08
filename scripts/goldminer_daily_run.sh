b5c8bd6fa5205ef7d811c8a8ec0ad188d1732d0b
abing <binglianghuang@gmail.com>
2019-04-08 08:37:49 +0800

    run scripts

--- scripts/goldminer_daily_run.sh
+++ scripts/goldminer_daily_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-path="/usr/local/anaconda3/bin/"
+path="/home/abing/goldminer/scripts"
 
 python $path/goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
 python $path/goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
