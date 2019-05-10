eb2245db57530563b9b5b7d07f5619fade1cf1c1
abing <binglianghuang@gmail.com>
2019-05-10 08:25:47 +0800

    change python path

--- scripts/goldminer_daily_run.sh
+++ scripts/goldminer_daily_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 path="/home/abing/goldminer/scripts"
+PYTHON="/usr/local/anaconda3/bin/python"
 
-python $path/goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
-python $path/goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
-python $path/goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
-python $path/goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
+$PYTHON $path/goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
+$PYTHON $path/goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
+$PYTHON $path/goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
+$PYTHON $path/goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
