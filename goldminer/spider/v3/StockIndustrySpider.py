af4a6cab150b285636c575c92b49551bab90b2de
abing <binglianghuang@gmail.com>
2019-08-24 22:28:35 +0800

    Optimize industry spider

--- goldminer/spider/v3/StockIndustrySpider.py
+++ goldminer/spider/v3/StockIndustrySpider.py
@@ -1,45 +1,57 @@
 # coding: utf-8
 import json
 
+from goldminer.common.logger import get_logger
 from goldminer.spider.v3.GMBaseSpiderV3 import GMBaseSpiderV3
-from goldminer.storage import IndustryDao
-from goldminer.storage import StockDao
+from goldminer.storage.IndustryDao import IndustryDao
+from goldminer.storage.StockDao import StockDao
 
 '''
 从掘金的getIndustry接口下载stock的industy数据
 tushare的数据质量太低
 '''
 
+logger = get_logger(__name__)
 
-class StockSpider(GMBaseSpiderV3):
+
+class StockIndustrySpider(GMBaseSpiderV3):
     def __init__(self):
-        super(StockSpider, self).__init__()
+        super(StockIndustrySpider, self).__init__()
         self.stockDao = StockDao()
         self.industryDao = IndustryDao()
 
     def updateIndustry(self):
         industries = self.industryDao.all()
+        logger.info("[StockIndustrySpider] Get {} industries.".format(len(industries)))
+
+        stocksExistsDB = self.stockDao.all()
+        stocksDict = {}
+        for stock in stocksExistsDB:
+            stocksDict[stock.code] = stock
+
         for industry in industries:
             stocks = self.getIndustry(industry.code)
+            logger.info("[StockIndustrySpider] Stocks in industry {} are {}.".format(industry.name, stocks))
             for code in stocks:
                 code = code[5:]
-                stock = self.stockDao.getByCode(code)
-                if stock:
-                    if stock.industry is None or stock.industry == "":
-                        stock.industry = []
-                    else:
-                        print(stock)
-                        stock.industry = json.loads(stock.industry)
+                if code not in stocksDict:
+                    continue
 
-                    if industry.code not in stock.industry:
-                        stock.industry.append(industry.code)
-                        stock.industry = json.dumps(stock.industry)
-                        self.stockDao.add(stock)
-                        print("update industry: ", stock)
+                stock = stocksDict[code]
+                if stock.industry is None or stock.industry == "":
+                    stock.industry = []
+                else:
+                    stock.industry = json.loads(stock.industry)
+
+                if industry.code not in stock.industry:
+                    stock.industry.append(industry.code)
+                    stock.industry = json.dumps(stock.industry)
+                    self.stockDao.add(stock)
+                    logger.info("[StockIndustrySpider] Add industry {} for stock {}.".format(industry, stock))
 
 
 if __name__ == "__main__":
-    spider = StockSpider()
+    spider = StockIndustrySpider()
     spider.updateIndustry()
 
 
