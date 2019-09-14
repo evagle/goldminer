84510ec93feefb70dadc527d41756f6b309dc205
abing <binglianghuang@gmail.com>
2019-09-14 22:44:55 +0800

    remove get field and cache function from stock manager

--- goldminer/indicators/StockManager.py
+++ goldminer/indicators/StockManager.py
@@ -16,25 +16,6 @@ class StockManager:
         self.fundamentalsDao = StockFundamentalsDao()
         self.stockDao = StockDao()
 
-        '''
-        {
-            '000001' : 
-            {
-                "pe" : 
-                {
-                     "date1" : v1, 
-                     ...
-                }, 
-                "pb" : 
-                {
-                     "date2" : v2
-                     ...      
-                }
-            }
-        }
-        '''
-        self.__stockCache = {}
-
         # load trade dates when initiate
         self.__loadTradeDates()
 
@@ -45,75 +26,6 @@ class StockManager:
             return TradingDerivativeIndicator.PB
         elif field == "TotalMarketValue":
             return TradingDerivativeIndicator.TOTMKTCAP
-
-    def __preLoadTradingDerivative(self):
-        if len(self.__stockCache) > 0:
-            print("Cache for TradingDerivativeIndicator already loaded.")
-            return
-
-        start = time.clock()
-
-        session = self.fundamentalsDao.getSession()
-        result = session.query(TradingDerivativeIndicator.code,
-                               TradingDerivativeIndicator.end_date,
-                               TradingDerivativeIndicator.PETTM,
-                               TradingDerivativeIndicator.PB,
-                               TradingDerivativeIndicator.TOTMKTCAP)
-        for row in result:
-            # cache[code][trade_date] = {fields}
-            code = row[0]
-            date = row[1]
-            if code not in self.__stockCache:
-                self.__stockCache[code] = {}
-
-            self.__stockCache[code][date] = {
-                "PETTM": row[2],
-                "PB": row[3],
-                "TotalMarketValue": row[4],
-            }
-        end = time.clock()
-        print(
-            "Load {} TradingDerivativeIndicator PE,PB,TOTMKTCAP successfully, time = {}".format(len(self.__stockCache),
-                                                                                                end - start))
-
-    def __loadStockField(self, stockCode, field):
-        if stockCode in self.__stockCache and field in self.__stockCache[stockCode]:
-            return
-
-        if stockCode not in self.__stockCache:
-            self.__stockCache[stockCode] = {}
-
-        session = self.fundamentalsDao.getSession()
-        fieldColumn = self.getFieldByName(field)
-        result = session.query(TradingDerivativeIndicator.end_date, fieldColumn) \
-            .filter(TradingDerivativeIndicator.code == stockCode)
-
-        data = {}
-        for row in result:
-            data[row[0].strftime("%Y-%m-%d")] = row[1]
-        self.__stockCache[stockCode][field] = data
-        print("[%s] Load %d %s successfully" % (stockCode, len(data), field))
-
-    def getStockFieldByDate(self, code, field, d: date):
-        if code not in self.__stockCache:
-            self.__preLoadTradingDerivative()
-
-        if code in self.__stockCache and d in self.__stockCache[code] and field in self.__stockCache[code][d]:
-            return self.__stockCache[code][d][field]
-        else:
-            return 0
-
-    def getStockPETTM(self, stockCode, d: date):
-        field = "PETTM"
-        return self.getStockFieldByDate(stockCode, field, d)
-
-    def getStockPB(self, stockCode, d: date):
-        field = "PB"
-        return self.getStockFieldByDate(stockCode, field, d)
-
-    def getStockTotalMarketValue(self, stockCode, d: date):
-        field = "TotalMarketValue"
-        return self.getStockFieldByDate(stockCode, field, d)
 
     def __loadTradeDates(self):
         session = self.fundamentalsDao.getSession()
