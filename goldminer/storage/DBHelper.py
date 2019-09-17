5e23446d5a28a12b6294903fcb38472e8dc4f888
Bingliang <blhuang@amazon.com>
2019-09-17 15:27:41 +0800

    Fix db host and port

--- goldminer/storage/DBHelper.py
+++ goldminer/storage/DBHelper.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 class DBHelper:
-    __engine = create_engine('mysql+pymysql://pocketpet:3MjdvUuXqxca6cbc@localhost:3307/goldminer?charset=utf8',
+    __engine = create_engine('mysql+pymysql://pocketpet:3MjdvUuXqxca6cbc@unionfight.citypet.cn:3306/goldminer?charset=utf8',
                            pool_recycle=60)
     __DBSessionClass = sessionmaker(bind=__engine)
     __session = __DBSessionClass()
-    __pymysql_conn = pymysql.connect(host="localhost", port=3307, user='pocketpet', password='3MjdvUuXqxca6cbc', db='goldminer')
+    __pymysql_conn = pymysql.connect(host="unionfight.citypet.cn", port=3306, user='pocketpet', password='3MjdvUuXqxca6cbc', db='goldminer')
 
     @staticmethod
     def getSession() -> Session:
