a2de73ea99242f6823a56056137d2fcde91fa84f
abing <binglianghuang@gmail.com>
2019-09-22 10:29:13 +0800

    Bugfix: pe/pb height is wrong when incremental build

--- goldminer/indicators/IndexPEPBHeightProcessor.py
+++ goldminer/indicators/IndexPEPBHeightProcessor.py
@@ -2,7 +2,10 @@
 from datetime import timedelta, datetime
 from decimal import Decimal
 
+from goldminer.common.logger import get_logger
 from goldminer.indicators.IndexPEPBBaseProcessor import IndexPEPBBaseProcessor
+
+logger = get_logger(__name__)
 
 
 class IndexPEPBHeightProcessor(IndexPEPBBaseProcessor):
@@ -33,36 +36,30 @@ class IndexPEPBHeightProcessor(IndexPEPBBaseProcessor):
         self.fieldName = self.baseFieldName
 
     def process(self, indexCode):
-        d = self.getStartDate(indexCode)
-        print("[%s] Update %s from %s" % (indexCode, self.heightFieldName, d))
-
-        if d is None:
-            print("[Error] [%s] Invalid start date." % indexCode)
-            return
+        logger.info("[{}] Start processing {} ".format(indexCode, self.heightFieldName))
 
         indicators = self.indexPrimaryIndicatorDao.getByCode(indexCode)
         if len(indicators) == 0:
-            print("[%s] no data found." % indexCode)
+            logger.error("[{}] No primary indicators found.".format(indexCode))
             return
 
         changed = []
         queue = []
         date_queue = []
         for current in indicators:
-            # If field(w_pb/pe_height_ten_year) is not None, no need to calculate
-            if getattr(current, self.heightFieldName) is not None:
-                continue
-            # If base field(weighted_pb/pe) is None, no need to calculate
-            if getattr(current, self.baseFieldName) is None:
-                continue
-
-            if hasattr(current, self.baseFieldName) and getattr(current, self.baseFieldName) is not None:
+            if getattr(current, self.baseFieldName) is not None:
                 val = getattr(current, self.baseFieldName)
                 queue.append(val)
                 date_queue.append(current.trade_date)
                 if (current.trade_date - date_queue[0]).days > 3650:
                     queue.pop(0)
                     date_queue.pop(0)
+            else:
+                continue
+
+            # If field(w_pb/pe_height_ten_year) is not None, no need to calculate
+            if getattr(current, self.heightFieldName) is not None:
+                continue
 
             totalCount = len(queue)
             smallerCount = 0
@@ -78,19 +75,20 @@ class IndexPEPBHeightProcessor(IndexPEPBBaseProcessor):
             setattr(current, self.heightFieldName, height)
             changed.append(current)
 
-            print(
-                "[%s] %s %s pe=%f, less=%d, total=%d, height=%f" % (indexCode, self.heightFieldName, current.trade_date,
-                                                                    getattr(current, self.baseFieldName), smallerCount,
-                                                                    totalCount, height))
+            logger.info("[{}] New value for {}, date={}, base={}, height={}({}/{})".format(
+                indexCode, self.heightFieldName, current.trade_date, getattr(current, self.baseFieldName),
+                height, smallerCount, totalCount
+            ))
 
         if len(changed) > 0:
             self.indexPrimaryIndicatorDao.bulkSave(changed)
-            print("[%s] %d %s updated" % (indexCode, len(changed), self.heightFieldName))
-
+            logger.info("[{}] {} value updated for field {}".format(indexCode, len(changed), self.heightFieldName))
+        else:
+            logger.info("[{}] {} is up to date".format(indexCode, self.heightFieldName))
         return 0
 
 
 if __name__ == "__main__":
     manager = IndexPEPBHeightProcessor()
     manager.runWeightedPBHeight()
-    manager.process('399001')
+    manager.process('000001')
