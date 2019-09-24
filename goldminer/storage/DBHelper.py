9c570b0f3e918d77be2d38106b4225a097699769
Bingliang <blhuang@amazon.com>
2019-09-24 16:20:01 +0800

    set autoflush and expire_on_commit=False

--- goldminer/storage/DBHelper.py
+++ goldminer/storage/DBHelper.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 class DBHelper:
     __engine = create_engine('mysql+pymysql://pocketpet:3MjdvUuXqxca6cbc@unionfight.citypet.cn:3306/goldminer?charset=utf8',
                            pool_recycle=60)
-    __DBSessionClass = sessionmaker(bind=__engine)
+    __DBSessionClass = sessionmaker(bind=__engine, autoflush=False, autocommit=False, expire_on_commit=False)
     __session = __DBSessionClass()
     __pymysql_conn = pymysql.connect(host="unionfight.citypet.cn", port=3306, user='pocketpet', password='3MjdvUuXqxca6cbc', db='goldminer')
 
