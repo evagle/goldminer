b4314bbb68617a1ec755bcfa758256828adacb93
Bingliang <blhuang@amazon.com>
2019-09-25 12:14:00 +0800

    Format log

--- goldminer/spider/v3/IndexWeightsSpider.py
+++ goldminer/spider/v3/IndexWeightsSpider.py
@@ -3,11 +3,13 @@ import json
 import time
 from datetime import timedelta, datetime
 
-from goldminer.common.Utils import Utils
+from goldminer.common.logger import get_logger
 from goldminer.models.models import IndexWeight
 from goldminer.spider.v3.GMBaseSpiderV3 import GMBaseSpiderV3
 from goldminer.storage.IndexWeightDao import IndexWeightDao
 from goldminer.storage.IndexesDao import IndexesDao
+
+logger = get_logger(__name__)
 
 
 class IndexWeightsSpider(GMBaseSpiderV3):
@@ -27,7 +29,7 @@ class IndexWeightsSpider(GMBaseSpiderV3):
         startDate = self.indexWeightDao.getLatestDate(code) + timedelta(days=1)
         endDate = datetime.now() + timedelta(days=1)
 
-        print("[Download weights ] start=", startDate, "end=", endDate, "code=", self.getIndexSymbol(code) + "." + code)
+        logger.info("[{}] Starts downloading weights from {} to {}".format(code, startDate, endDate))
         results = self.getHistoryConstituents(index=self.getIndexSymbol(code) + "." + code, start_date=startDate,
                                               end_date=endDate)
 
@@ -36,32 +38,9 @@ class IndexWeightsSpider(GMBaseSpiderV3):
             if item['trade_date'].date() >= startDate:
                 models.append(self.rawDataToModel(code, item))
 
-        print("[Download weights ] count = ", len(models), "\n")
+        logger.info("[{}] End downloading weights, {} downloaded.".format(code, len(models)))
         self.indexWeightDao.addAll(models)
         return models
-
-    def checkAndUpdateLatestConstituents(self, code):
-        result = self.getConstituents(self.codeToIndexSymbol(code), "symbol, weight")
-        dic = {}
-        for item in result:
-            dic[item["symbol"]] = item['weight']
-
-        today = datetime.now().date()
-        last = self.indexWeightDao.getByDate(code, today)
-        if last is None:
-            if len(dic) == 0:
-                print("[%s] has no index weight found")
-            else:
-                print(code, last, dict)
-        elif not Utils.isDictEqual(dic, json.loads(last.constituents)):
-            model = IndexWeight()
-            model.code = code
-            model.constituents = json.dumps(dic)
-            model.trade_date = today
-            self.indexWeightDao.add(model)
-            print("[%s] new index weight record" % code)
-        else:
-            print("[%s] index weight is up to date." % code)
 
     def downloadAllIndexConstituents(self):
         if datetime.now().day < 25:
@@ -71,6 +50,7 @@ class IndexWeightsSpider(GMBaseSpiderV3):
         for code in indexes:
             self.downloadConstituents(code)
             time.sleep(0.1)
+
 
 if __name__ == "__main__":
     spider = IndexWeightsSpider()
