1197b9c06440c601fdc2ed5f270961d3acfdb239
Bingliang <blhuang@amazon.com>
2019-09-26 12:38:11 +0800

    Constituent download process optimize

--- goldminer/spider/CSIndexSpider.py
+++ goldminer/spider/CSIndexSpider.py
@@ -41,7 +41,7 @@ class CSIndexSpider:
                 sheet = workbook.sheet_by_index(0)
                 cols = sheet.col_values(4)
                 cols.remove('成分券代码Constituent Code')
-                tradeDate = sheet.col_values(0)[1]
+                tradeDate = Utils.parseConstituentUpdateDate(sheet.col_values(0)[1])
                 return [tradeDate, cols]
 
     def isCSIndex(self, code):
@@ -49,53 +49,61 @@ class CSIndexSpider:
         return model.source == GMConsts.CS_INDEX or code[0:2] == "00" or model.publisher == "中证公司"
 
     def checkAndUpdateLatestConstituents(self, code):
+        """
+        1. Fetch latest bar from database
+            1.1 if None, insert the new one
+            1.2 if latest.trade_date < new.trade_date,  insert the new one
+            1.3 if latest.trade_date = new.trade_date, compare constituents and update
+
+        :param code:
+        :return:
+        """
         if not self.isCSIndex(code):
-            logger.info("[%s] not CSIndex index" % code)
+            logger.info("[{}] not CSIndex index".format(code))
             return
 
         result = self.fetchConstituentByCode(code)
-        if result is None:
-            logger.info("[%s] no new constituent found" % code)
+
+        if result is None or len(result[1]) is None:
+            logger.info("[{}] no new constituent found".format(code))
+            return
+        if result[0] is None:
+            logger.info("[{}] trade date is none.".format(code))
             return
 
         tradeDate = result[0]
         newConstituents = result[1]
 
-        today = datetime.now().date()
-        last = self.constituentsDao.getConstituents(code, today)
-        if last is None:
-            if len(newConstituents) == 0:
-                logger.info("[%s] has no constituent found" % code)
-            else:
-                model = IndexConstituent()
-                model.code = code
-                model.constituents = json.dumps(newConstituents)
-                model.trade_date = tradeDate
-                model.source = GMConsts.CS_INDEX
-                self.constituentsDao.add(model)
-                logger.info("[%s] not last found. Add first one, %s" % (code, model))
-        elif not Utils.isListEqual(newConstituents, json.loads(last.constituents)):
-            model = self.constituentsDao.getByDate(code, tradeDate)
-            if model is None:
-                model = IndexConstituent()
+        latest = self.constituentsDao.getConstituentsBeforeDate(code, datetime.today().date())
+        if latest is None or latest.trade_date < tradeDate:
+            model = IndexConstituent()
             model.code = code
             model.constituents = json.dumps(newConstituents)
             model.trade_date = tradeDate
             model.source = GMConsts.CS_INDEX
             self.constituentsDao.add(model)
-            logger.info("[%s] new constituents date = %s" % (code, model.trade_date))
-
+            logger.info("[{}] insert new constituent {}".format(code, model))
+        elif latest.trade_date == tradeDate:
+            if not Utils.isListEqual(newConstituents, json.loads(latest.constituents)):
+                latest.code = code
+                latest.constituents = json.dumps(newConstituents)
+                latest.trade_date = tradeDate
+                latest.source = GMConsts.CS_INDEX
+                self.constituentsDao.add(latest)
+                logger.info("[{}] new constituents {}".format(code, latest))
+            else:
+                logger.info("[{}] constituent in database is up to date. trade date={}".format(code, tradeDate))
         else:
-            logger.info("[%s] constituent is up to date." % code)
+            logger.info("[{}] download an older constituents. trade date={}".format(code, tradeDate))
 
     def checkAndUpdateAllLatestConstituents(self):
         indexes = self.indexesDao.getIndexList()
         for code in indexes:
             if self.isCSIndex(code):
                 self.checkAndUpdateLatestConstituents(code)
-                time.sleep(1)
+                time.sleep(0.1)
 
 
 if __name__ == "__main__":
     spider = CSIndexSpider()
-    spider.checkAndUpdateLatestConstituents('399989')
+    spider.checkAndUpdateAllLatestConstituents()
