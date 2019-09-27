a6c5955f96484b0444a3a12a685c9a90039855c3
Bingliang <blhuang@amazon.com>
2019-09-27 11:04:25 +0800

    Upgrade RPS processor

--- goldminer/indicators/RPSProcessor.py
+++ goldminer/indicators/RPSProcessor.py
@@ -14,58 +14,71 @@ class RPSProcessor:
     def __init__(self):
         self.customIndicatorDao = StockCustomIndicatorDao()
 
+    def hasRPS(self, bars):
+        """
+        Given a list of bars of one day,
+        if 90% of the bars have rps, then we can think rps was already generated for that day,
+        else the rps is not generated
+        :param bars: all bars for one day
+        :return: true if 90% bars have rps else false
+        """
+        if len(bars) == 0:
+            return False
+
+        n = 0
+        for bar in bars:
+            if bar.rps50 is not None and bar.rps120 is not None and bar.rps250 is not None:
+                n += 1
+        return n / len(bars) > 0.9
+
+    def calculateRPS(self, bars, period):
+        validBars = []
+        rpsName = 'rps' + str(period)
+        gainName = 'gain' + str(period)
+        MIN = -1e6
+        for bar in bars:
+            if getattr(bar, gainName):
+                validBars.append(bar)
+            else:
+                setattr(bar, rpsName, 0)
+
+        if len(validBars) / len(bars) < 0.9:
+            logger.warning("All bars have no enough(at least 90%) gain{} value".format(period))
+            return False
+
+        sortedBars = sorted(validBars,
+                            key=lambda bar: getattr(bar, gainName) if getattr(bar, gainName) is not None else MIN,
+                            reverse=True)
+        count = len(sortedBars)
+
+        for i in range(count):
+            bar = sortedBars[i]
+            rps = 100 - (i + 1) * 100 / count
+            setattr(bar, rpsName, rps)
+
+        logger.info("{} rps{} value updated".format(len(bars), period))
+        return True
+
     def process(self, trade_date):
         logger.info("Start processing rps for date {}".format(trade_date))
 
         bars = list(self.customIndicatorDao.getAllAtDate(trade_date).values())
-        # 过滤掉B股
-        left = []
-        for bar in bars:
-            if not bar.code.startswith("9"):
-                left.append(bar)
-        bars = left
 
-        total_count = len(bars)
-        MIN = -1e6
+        if len(bars) == 0:
+            logger.info("0 bars found at date: {}".format(trade_date))
+            return None
 
-        has_rps = True
-        for bar in bars:
-            if bar.rps50 is None or bar.rps120 is None or bar.rps250 is None:
-                has_rps = False
-            if bar.gain50 is None or bar.gain120 is None or bar.gain120 is None:
-                logger.info('gain50,gain120,gain250 are not ready')
-                return None
-
-        if has_rps:
+        if self.hasRPS(bars):
             logger.info("RPS had already been calculated for trade_date {}".format(trade_date))
             return None
 
-        bars50 = sorted(bars, key=lambda bar: bar.gain50 if bar.gain50 is not None else MIN, reverse=True)
-        for i in range(total_count):
-            bar = bars50[i]
-            if bar.gain50 is None:
-                bar.rps50 = 0
-            else:
-                bar.rps50 = 100 - (i + 1) * 100 / total_count
+        updated = False
+        for period in [50, 120, 250]:
+            status = self.calculateRPS(bars, period)
+            updated = updated or status
 
-        bars120 = sorted(bars, key=lambda bar: bar.gain120 if bar.gain120 is not None else MIN, reverse=True)
-        for i in range(total_count):
-            bar = bars120[i]
-            if bar.gain120 is None:
-                bar.rps120 = 0
-            else:
-                bar.rps120 = 100 - (i + 1) * 100 / total_count
-
-        bars250 = sorted(bars, key=lambda bar: bar.gain250 if bar.gain250 is not None else MIN, reverse=True)
-        for i in range(total_count):
-            bar = bars250[i]
-            if bar.gain250 is None:
-                bar.rps250 = 0
-            else:
-                bar.rps250 = 100 - (i + 1) * 100 / total_count
-
-        logger.info("{} rps bars updated".format(len(bars)))
-        self.customIndicatorDao.bulkSave(bars)
+        if updated:
+            self.customIndicatorDao.bulkSave(bars)
         logger.info("End processing rps for date {}".format(trade_date))
 
     def getLastRPSDate(self):
