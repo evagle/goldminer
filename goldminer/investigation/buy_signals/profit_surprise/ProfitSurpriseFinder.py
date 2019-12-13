a689be10ae7fb3618e71db1e7e44379cbb1dfb77
abing <binglianghuang@gmail.com>
2019-12-14 01:10:29 +0800

    业绩预告和业绩快报下载

--- goldminer/investigation/buy_signals/profit_surprise/ProfitSurpriseFinder.py
+++ goldminer/investigation/buy_signals/profit_surprise/ProfitSurpriseFinder.py
@@ -7,6 +7,7 @@ from goldminer.spider.tushare.TSStockBarSpider import TSStockBarSpider
 from goldminer.storage.DerivativeFinanceIndicatorDao import DerivativeFinanceIndicatorDao
 
 from goldminer.storage.PerformanceForecastDao import PerformanceForecastDao
+from goldminer.storage.PerformancePreviewDao import PerformancePreviewDao
 from goldminer.storage.StockDailyBarAdjustNoneDao import StockDailyBarAdjustNoneDao
 from goldminer.storage.StockDao import StockDao
 
@@ -14,6 +15,7 @@ from goldminer.storage.StockDao import StockDao
 class ProfitSurpriseFinder:
     def __init__(self):
         self.forecastDao = PerformanceForecastDao()
+        self.previewDao = PerformancePreviewDao()
         self.stockBarDao = StockDailyBarAdjustNoneDao()
         self.derivativeFinanceIndicatorDao = DerivativeFinanceIndicatorDao()
         self.stockDao = StockDao()
@@ -31,7 +33,7 @@ class ProfitSurpriseFinder:
         if model is None:
             return None
 
-        if (date - model.pub_date).days > 7:
+        if (date - model.pub_date).days > 8:
             return None
 
         return model
@@ -41,7 +43,17 @@ class ProfitSurpriseFinder:
         if model is None:
             return None
 
-        if (date - model.pub_date).days > 7:
+        if (date - model.pub_date).days > 8:
+            return None
+
+        return model
+
+    def find_preview_before_date(self, code, date):
+        model = self.previewDao.getFirstWithPubDateBefore(code, date)
+        if model is None:
+            return None
+
+        if (date - model.pub_date).days > 8:
             return None
 
         return model
@@ -70,79 +82,40 @@ class ProfitSurpriseFinder:
             if (next_bar.close > bar.close * 1.04 and next_bar.low > bar.high ) or \
                 (next_bar.close > bar.close * 1.09):
                 # 业绩预告
-                forecast = self.find_forecast_before_date(code, next_bar.trade_date.date())
-                if forecast:
-                    if lastSurprise is not None and lastSurprise.pub_date == forecast.pub_date:
-                        self.logger.info("Surprise exists")
-                        continue
-                    surprise = ProfitSurprise()
-                    surprise.code = code
-                    surprise.trade_date = next_bar.trade_date
-                    surprise.pub_date = forecast.pub_date
-                    surprise.profit_growth_low = forecast.growth_rate_low
-                    surprise.profit_growth_high = forecast.growth_rate_high
-                    surprise.price_gap = 1 if next_bar.low > bar.high else 0
-                    surprise.price_increase = next_bar.close / bar.close
-                    # self.forecastDao.insertOrReplace(surprise)
-                    print(surprise.code, surprise.trade_date, surprise.pub_date)
-                    lastSurprise = surprise
+                performanceAnnouncement = {
+                    "forecast": self.find_forecast_before_date(code, next_bar.trade_date.date()),
+                    "preview": self.find_preview_before_date(code, next_bar.trade_date.date()),
+                    "derivative": self.find_derivative_finance_indicator_before_date(code, next_bar.trade_date.date())
+                }
 
-                # TODO 业绩快报
+                for performanceType, report in performanceAnnouncement.items():
+                    if report:
+                        if lastSurprise is not None and lastSurprise.pub_date == report.pub_date:
+                            self.logger.info("Surprise exists")
+                            continue
+                        surprise = ProfitSurprise()
+                        surprise.code = code
+                        surprise.trade_date = next_bar.trade_date
+                        surprise.pub_date = report.pub_date
+                        if performanceType == 'forecast':
+                            surprise.profit_growth_low = report.growth_rate_low
+                            surprise.profit_growth_high = report.growth_rate_high
+                        elif performanceType == 'preview':
+                            surprise.profit_growth_low = report.profit_growth
+                            surprise.profit_growth_high = report.profit_growth
+                        elif performanceType == 'derivative':
+                            surprise.profit_growth_low = report.NPGRT
+                            surprise.profit_growth_high = report.NPGRT
+                        surprise.price_gap = 1 if next_bar.low > bar.high else 0
+                        surprise.price_increase = next_bar.close / bar.close
+                        # self.forecastDao.insertOrReplace(surprise)
+                        print(surprise.code, surprise.trade_date, surprise.pub_date)
+                        lastSurprise = surprise
 
-                # 财报
-                derivativeFinance = self.find_derivative_finance_indicator_before_date(code, next_bar.trade_date.date())
-                print(code, next_bar.trade_date.date(), derivativeFinance)
-
-
-                if derivativeFinance:
-                    if lastSurprise is not None and lastSurprise.pub_date == derivativeFinance.pub_date:
-                        self.logger.info("Surprise exists")
-                        continue
-                    surprise = ProfitSurprise()
-                    surprise.code = code
-                    surprise.trade_date = next_bar.trade_date
-                    surprise.pub_date = derivativeFinance.pub_date
-                    surprise.profit_growth_low = derivativeFinance.NPGRT
-                    surprise.profit_growth_high = derivativeFinance.NPGRT
-                    surprise.price_gap = 1 if next_bar.low > bar.high else 0
-                    surprise.price_increase = next_bar.close / bar.close
-                    # self.forecastDao.insertOrReplace(surprise)
-                    print(surprise.code, surprise.trade_date, surprise.pub_date)
-                    lastSurprise = surprise
 
     def run(self):
 
-        stocks = [
-            # '600183',  # 生益科技7月24 业绩快报
-            # '000049',  # 德赛电池，724 业绩快报
-            # '002045',  # 国光电器，715 业绩快报
-            # '603008',  # 喜临门，724 断层当天收绿，业绩快报
-
-
-            # '002791',  # 坚郎五金，8.28 断层当天收绿，且只上涨0.78% 不考虑了 N
-            # '300319',  # 麦捷科技，10.14 业绩预告没下载下来
-
-            # '300413',  # 芒果超媒，8.30 断层当天收绿，且只上涨4% Y
-            # '300702',  # 天宇股份，8.22 断层当天收绿 Y
-            # '603936',  # 博敏电子，7.24 Y
-            # '002605',  # 姚记科技，10.15 Y
-            # '002850',  # 科达利，715 Y
-            # '600745',  # 闻泰科技，830 Y
-            # '002396',  # 星网锐捷，711 Y
-            # '002351',  # 漫步者，8.8 Y
-            # '603129',  # 春风动力，823 Y
-            # '002463',  # 沪电股份，626  Y
-            # '002705',  # 新宝股份，8.28 Y
-            # '300014',  # 亿纬锂能，10.8 Y
-            # '300558',  # 贝达药业，10.11 Y
-            # '300632',  # 光蒲股份，7.12 Y
-            # '300661',  # 圣邦股份，7.9 Y
-            # '603882',  # 金域医学，8.2 Y
-            # '603658',  # 安图生物，8.9 Y
-            # '300363',  # 博腾股份，7.31 Y
-            # '002124',  # 天邦股份，10.8 Y
-            # '002458',  # 益生股份，10.9 Y
-            ]  # stockDao.getStockList()
+        stocks = self.stockDao.getStockList()
         surprises = []
         for code in stocks:
             self.findSurpurise(code)
@@ -152,3 +125,35 @@ class ProfitSurpriseFinder:
 if __name__ == "__main__":
     profit = ProfitSurpriseFinder()
     profit.run()
+
+    # For debugging
+    surprises = [
+        '002791',  # 坚郎五金，8.28 断层当天收绿，且只上涨0.78% 不考虑了 N
+
+        '300319',  # 麦捷科技，10.14 Y
+        '600183',  # 生益科技7月25 业绩快报 Y
+        '000049',  # 德赛电池，724 业绩快报 Y
+        '002045',  # 国光电器，715 Y
+        '603008',  # 喜临门，724 断层当天收绿，业绩快报 Y
+        '300413',  # 芒果超媒，8.30 断层当天收绿，且只上涨4% Y
+        '300702',  # 天宇股份，8.22 断层当天收绿 Y
+        '603936',  # 博敏电子，7.24 Y
+        '002605',  # 姚记科技，10.15 Y
+        '002850',  # 科达利，715 Y
+        '600745',  # 闻泰科技，830 Y
+        '002396',  # 星网锐捷，711 Y
+        '002351',  # 漫步者，8.8 Y
+        '603129',  # 春风动力，823 Y
+        '002463',  # 沪电股份，626  Y
+        '002705',  # 新宝股份，8.28 Y
+        '300014',  # 亿纬锂能，10.8 Y
+        '300558',  # 贝达药业，10.11 Y
+        '300632',  # 光蒲股份，7.12 Y
+        '300661',  # 圣邦股份，7.9 Y
+        '603882',  # 金域医学，8.2 Y
+        '603658',  # 安图生物，8.9 Y
+        '300363',  # 博腾股份，7.31 Y
+        '002124',  # 天邦股份，10.8 Y
+        '002458',  # 益生股份，10.9 Y
+    ]
+
--- goldminer/spider/eastmoney/PerformancePreviewSpider.py
+++ goldminer/spider/eastmoney/PerformancePreviewSpider.py
@@ -7,11 +7,11 @@ import time
 import requests
 
 from goldminer.common.logger import get_logger
-from goldminer.models.models import PerformanceForecast
-from goldminer.storage.PerformanceForecastDao import PerformanceForecastDao
+from goldminer.models.models import PerformancePreview
+from goldminer.storage.PerformancePreviewDao import PerformancePreviewDao
 
 
-class ForecastSpider():
+class PerformancePreviewSpider():
     def __init__(self):
         self.__logger = get_logger(__name__)
         self.__headers = {
@@ -22,9 +22,9 @@ class ForecastSpider():
             "Referer": "http://data.eastmoney.com/bbsj/201803/yjyg.html",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36",
         }
-        self.__forcast_url = "http://dcfm.eastmoney.com//em_mutisvcexpandinterface/api/js/get"
+        self.__base_url = "http://dcfm.eastmoney.com//em_mutisvcexpandinterface/api/js/get"
 
-        self.forecastDao = PerformanceForecastDao()
+        self.previewDao = PerformancePreviewDao()
 
         self.__force_scan = False
 
@@ -34,7 +34,7 @@ class ForecastSpider():
     def run(self):
         year = datetime.datetime.now().year
         # 业绩预告的开启时间
-        forecast_window_config = [
+        preview_window_config = [
             {
                 "end_date": datetime.datetime(year - 1, 12, 31),
                 "start": datetime.datetime(year - 1, 9, 30),
@@ -62,8 +62,8 @@ class ForecastSpider():
             },
         ]
         today = datetime.datetime.today()
-        for window in forecast_window_config:
-            # Give 15 more days for each window to accept forecast updates
+        for window in preview_window_config:
+            # Give 15 more days for each window to accept preview updates
             if window['start'] <= today <= window['end'] + datetime.timedelta(days=15):
                 self.download_by_end_date(window['end_date'])
 
@@ -71,35 +71,36 @@ class ForecastSpider():
         if (end_date.month, end_date.day) not in [(3,31),(6,30),(9,30),(12,31),]:
             self.__logger.error("Wrong end date format: {}".format(end_date))
             return
-
         page = 1
         total_pages = 1
-        self.__logger.info("Start downloading forecast for end_date: {}".format(end_date))
-        visited = {}
+        self.__logger.info("Start downloading preview for end_date: {}".format(end_date))
         while page <= total_pages:
-            total_pages, new_forecast_ratio = self.download_page(page, end_date, visited)
+            result = self.download_page(page, end_date)
+            if result is None:
+                continue
+
+            total_pages, new_preview_ratio = result
             self.__logger.info("Download page {}/{} successfully for end_date {}".format(page, total_pages, end_date))
-            # stop if new forecast ratio is less than 5 percent in current page,
-            # which means > 95% forecasts in current page were in database already
-            if new_forecast_ratio < 0.05:
-                break
+            # stop if new preview ratio is less than 5 percent in current page,
+            # which means > 95% previews in current page were in database already
+            # if new_preview_ratio < 0.05:
+            #     break
             page += 1
 
-    def download_page(self, page, end_date, visited):
+    def download_page(self, page, end_date):
         enddate_str = end_date.strftime("%Y-%m-%d")
         params = {
-            "type": "YJBB21_YJYG",
-            "st": "ndate",
+            "type": "YJBB21_YJKB",
+            "st": "ldate",
             "token": "70f12f2f4f091e459a279469fe49eca5",
             "sr": "-1",
             "p": str(page),
-            "ps": "30",
-            "filter": "(IsLatest='T')(enddate=^{}^)".format(enddate_str),
-            "rt": "52521620",
-            "js": "var rsDGWjfi={\"pages\":(tp),\"data\":(x),\"font\":(font)}"
+            "ps": "50",
+            "filter": "(securitytypecode in ('058001001','058001002'))(rdate=^{}^)".format(enddate_str),
+            "rt": "52541700",
+            "js": "var eVCzgTPi={\"pages\":(tp),\"data\":(x),\"font\":(font)}"
         }
-
-        return self.call_eastmoney_forecast_js_api(self.__forcast_url, self.__headers, params, visited)
+        return self.call_eastmoney_preview_js_api(self.__base_url, self.__headers, params, {})
 
     def decode_numbers(self, string, font_mapping):
         for encoded_number in font_mapping:
@@ -107,62 +108,51 @@ class ForecastSpider():
             string = string.replace(encoded_number, decoded_value)
         return string
 
-    def make_forecast_model(self, item, font_mapping):
+    def make_preview_model(self, item, font_mapping):
         """
-        Accepting item and font_mapping, from eastmoney api, make a new PerformanceForecast model
+        Accepting item and font_mapping, from eastmoney api, make a new Performancepreview model
         :param item:
         :param font_mapping:
         :return:
         """
-        model = PerformanceForecast()
+        model = PerformancePreview()
         for key in item:
             value = self.decode_numbers(item[key], font_mapping)
+            if value == "-":
+                value = 0
+
             if key == "scode":
                 model.code = value
-            elif key == "enddate":
+            elif key == "rdate":
                 model.end_date = datetime.datetime.strptime(value[:10], "%Y-%m-%d").date()
-            elif key == "ndate":
+            elif key == "ldate":
                 model.pub_date = datetime.datetime.strptime(value[:10], "%Y-%m-%d").date()
-            elif key == "forecasttype":
-                model.forecast_type = value
-            elif key == "forecastcontent":
-                model.forecast_content = value
-            elif key == "forecastl":
-                model.profit_forecast_low = float(value)
-            elif key == "forecastt":
-                if value == "-":
-                    model.profit_forecast_high = model.profit_forecast_low
-                else:
-                    model.profit_forecast_high = float(value)
-            elif key == "yearearlier":
-                if value == "-":
-                    model.profit_last_year = 0
-                else:
-                    model.profit_last_year = float(value)
-            elif key == "increasel":
-                if value == "-":
-                    model.growth_rate_low = 0
-                else:
-                    model.growth_rate_low = float(value)
-            elif key == "increaset":
-                if value == "-":
-                    model.growth_rate_high = model.growth_rate_low
-                else:
-                    model.growth_rate_high = float(value)
-            elif key == "changereasondscrpt":
-                model.explanation = value
+            elif key == "yysr":
+                model.income = value
+            elif key == "qntqys":
+                model.income_previous = value
+            elif key == "ys":
+                model.income_growth = value
+            elif key == "jlr":
+                model.profit = float(value)
+            elif key == "qntqjlr":
+                model.profit_previous = float(value)
+            elif key == "lr":
+                model.profit_growth = float(value)
+            elif key == "roeweighted":
+                model.roe = float(value)
         return model
 
-    def call_eastmoney_forecast_js_api(self, url, headers, params, visited):
+    def call_eastmoney_preview_js_api(self, url, headers, params, visited):
         """
-        Call eastmoney api to get forecast messages
+        Call eastmoney api to get preview messages
         :param url:
         :param headers:
         :param params:
         :return:
         tuple (
             total pages,
-            new forecast ratio(=new count/total count)
+            new preview ratio(=new count/total count)
         )
         """
         response = requests.get(url, params, headers=headers)
@@ -173,24 +163,26 @@ class ForecastSpider():
         content = response.text[13:]
         json_content = json.loads(content)
 
+
         font = json_content['font']
         font_mapping = {}
+        if font is None or font['FontMapping'] is None:
+            self.__logger.info("font is null for url={}, params={}".format(url, params))
+            return None
+
         for item in font['FontMapping']:
             font_mapping[item['code']] = str(item['value'])
 
         data = json_content['data']
-        # total forecast count not be visited before
+        # total preview count not be visited before
         total_count = 0
         new_count = 0
         for item in data:
-            if item['forecastl'] == '-':
-                self.__logger.warn("Skip data with forecastl = '-', data = {}".format(item))
-                continue
             if item['scode'][:1] not in ['0', '3', '6']:
                 self.__logger.warn("Skip scode {}, data = {}".format(item['scode'], item))
                 continue
 
-            model = self.make_forecast_model(item, font_mapping)
+            model = self.make_preview_model(item, font_mapping)
 
             key = (model.code, model.end_date, model.pub_date)
             if key in visited:
@@ -200,28 +192,29 @@ class ForecastSpider():
 
             total_count += 1
             try:
-                self.forecastDao.add(model)
+                self.previewDao.add(model)
                 new_count += 1
             except Exception as e:
                 self.__logger.warn("Failed to adding model, error message: {}".format(e))
-                self.forecastDao.rollback()
+                self.previewDao.rollback()
 
-        self.__logger.info("Successfully download one page of performance forecast, forecast count={}".format(len(data)))
+        self.__logger.info("Successfully download one page of performance preview, preview count={}".format(len(data)))
         time.sleep(random.randint(40, 100) / 50)
 
-        self.__logger.info("New forecast count / total count(excludes visited) = {}/{}".format(new_count, total_count))
+        self.__logger.info("New preview count / total count(excludes visited) = {}/{}".format(new_count, total_count))
         if total_count > 0:
-            new_forecast_ratio = new_count / total_count
+            new_preview_ratio = new_count / total_count
         else:
-            new_forecast_ratio = 1
-        return json_content['pages'], new_forecast_ratio
+            new_preview_ratio = 1
+        return json_content['pages'], new_preview_ratio
 
 
 if __name__ == "__main__":
-    spider = ForecastSpider()
-    year = 2012
-    spider.download_by_end_date(datetime.datetime(year, 11, 30))
-    # spider.download_by_end_date(datetime.datetime(year, 9, 30))
-    # spider.download_by_end_date(datetime.datetime(year, 6, 30))
-    # spider.download_by_end_date(datetime.datetime(year, 3, 31))
-    spider.run()
+    spider = PerformancePreviewSpider()
+
+    for year in range(2019, 2009, -1):
+        spider.download_by_end_date(datetime.datetime(year, 3, 31))
+        spider.download_by_end_date(datetime.datetime(year, 6, 30))
+        spider.download_by_end_date(datetime.datetime(year, 9, 30))
+        spider.download_by_end_date(datetime.datetime(year, 12, 31))
+    # spider.run()
