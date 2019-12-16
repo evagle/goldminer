9f79c0c9b8ba23600dffb6b5d08a7ae36c4a323a
abing <binglianghuang@gmail.com>
2019-12-16 19:07:31 +0800

    Optimize tushare stock bar spider

--- goldminer/spider/tushare/TSStockBarSpider.py
+++ goldminer/spider/tushare/TSStockBarSpider.py
@@ -3,20 +3,25 @@ from datetime import datetime
 
 import tushare as ts
 
+from goldminer.common.Utils import Utils
 from goldminer.models.models import StockDailyBar
 from goldminer.spider.tushare.TushareBase import TushareBase
 
 
 class TSStockBarSpider(TushareBase):
+    def __init__(self):
+        super().__init__()
 
-    def getDailyBars(self, code, adj='qfq', start_date: datetime = None, end_date: datetime = None):
+    def download_bars_from_tushare(self, code,  start_date: datetime=None, end_date: datetime=None, adj='qfq'):
         if start_date is None:
             start_date = datetime(2005, 1, 1).strftime("%Y%m%d")
         if end_date is None:
             end_date = datetime.now().strftime("%Y%m%d")
 
         ts_code = self.to_ts_code(code)
-        df = ts.pro_bar(api=self.ts_pro_api, ts_code=ts_code, adj=adj, start_date=start_date, end_date=end_date)
+
+        df = ts.pro_bar(api=self.ts_pro_api, ts_code=ts_code, adj=adj, start_date=start_date, end_date=end_date,
+                        adjfactor=True)
 
         if df is None:
             return None
@@ -26,13 +31,15 @@ class TSStockBarSpider(TushareBase):
             bar = StockDailyBar()
             bar.code = code
             bar.trade_date = datetime.strptime(row.trade_date, "%Y%m%d")
-            bar.open = row.open
-            bar.close = row.close
-            bar.high = row.high
-            bar.low = row.low
-            bar.pre_close = row.pre_close
+            bar.open = Utils.formatFloat(row.open / bar.adj_factor, 2)
+            bar.close = Utils.formatFloat(row.close / bar.adj_factor, 2)
+            bar.high = Utils.formatFloat(row.high / bar.adj_factor, 2)
+            bar.low = Utils.formatFloat(row.low / bar.adj_factor, 2)
+            bar.pre_close = Utils.formatFloat(row.pre_close / bar.adj_factor, 2)
             bar.amount = row.amount
             bar.volume = row.vol
+            bar.adj_factor = row.adj_factor
+
             bars.append(bar)
 
         bars = sorted(bars, key=lambda bar: bar.trade_date, reverse=False)
@@ -41,5 +48,6 @@ class TSStockBarSpider(TushareBase):
 
 if __name__ == "__main__":
     spider = TSStockBarSpider()
-    bars = spider.getDailyBars('000001')
-    print(bars)
+    bars_qfq = spider.download_bars_from_tushare('000001')
+
+
