076796e185858630288d6528b0ab9817968b8298
abing <binglianghuang@gmail.com>
2019-12-19 00:09:04 +0800

    Optimize tushare spider

--- goldminer/spider/tushare/TSStockBarSpider.py
+++ goldminer/spider/tushare/TSStockBarSpider.py
@@ -12,7 +12,7 @@ class TSStockBarSpider(TushareBase):
     def __init__(self):
         super().__init__()
 
-    def download_bars_from_tushare(self, code,  start_date: datetime=None, end_date: datetime=None, adj='qfq'):
+    def download_bars_from_tushare(self, code, start_date: datetime = None, end_date: datetime = None, adj='qfq'):
         if start_date is None:
             start_date = datetime(2005, 1, 1).strftime("%Y%m%d")
         if end_date is None:
@@ -30,12 +30,15 @@ class TSStockBarSpider(TushareBase):
         for row in df.itertuples(index=False):
             bar = StockDailyBar()
             bar.code = code
-            bar.trade_date = datetime.strptime(row.trade_date, "%Y%m%d")
-            bar.open = Utils.formatFloat(row.open / bar.adj_factor, 2)
-            bar.close = Utils.formatFloat(row.close / bar.adj_factor, 2)
-            bar.high = Utils.formatFloat(row.high / bar.adj_factor, 2)
-            bar.low = Utils.formatFloat(row.low / bar.adj_factor, 2)
-            bar.pre_close = Utils.formatFloat(row.pre_close / bar.adj_factor, 2)
+            bar.trade_date = datetime.strptime(row.trade_date, "%Y%m%d").date()
+            attrs = ['open', 'close', 'high', 'low', 'pre_close']
+            for attr in attrs:
+                if adj == 'hfq':
+                    value = getattr(row, attr) / row.adj_factor
+                    setattr(bar, attr, Utils.formatFloat(value, 2))
+                else:
+                    setattr(bar, attr, getattr(row, attr))
+
             bar.amount = row.amount
             bar.volume = row.vol
             bar.adj_factor = row.adj_factor
@@ -48,6 +51,5 @@ class TSStockBarSpider(TushareBase):
 
 if __name__ == "__main__":
     spider = TSStockBarSpider()
-    bars_qfq = spider.download_bars_from_tushare('000001')
-
+    bars_qfq = spider.download_bars_from_tushare('000001', adj='none')
 
