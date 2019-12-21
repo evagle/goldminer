b663531a32302c4974e4b844633938688e7260ec
abing <binglianghuang@gmail.com>
2019-12-21 22:07:34 +0800

    Refactor

--- goldminer/export/tdx/ExportProfitSurprise.py
+++ goldminer/export/tdx/ExportProfitSurprise.py
@@ -6,23 +6,27 @@ class ExportProfitSurprise:
     def __init__(self):
         self.profit_surprise_dao = ProfitSurpriseDao()
 
-    def export(self, outpath):
+    def get_surprises(self):
         surprises = self.profit_surprise_dao.all()
         result = []
         for surprise in surprises:
             market = 1 if surprise.code[0:1] == "6" else 0
             result.append([str(market), surprise.code, surprise.trade_date.strftime("%Y%m%d"), "1"])
 
-        self.save_to_file(result, outpath)
+        return result
 
-    def save_to_file(self, data, outpath):
-        with open(outpath, "w") as writer:
+    def export(self, out_path):
+        result = self.get_surprises()
+        self.save_to_file(result, out_path)
+
+    def save_to_file(self, data, out_path):
+        with open(out_path, "w") as writer:
             lines = []
             for row in data:
-                lines.append("|".join(row)+"\n")
+                lines.append("|".join(row) + "\n")
             writer.writelines(lines)
 
 
 if __name__ == "__main__":
     export_helper = ExportProfitSurprise()
-    data = export_helper.export("/Users/abing/Downloads/ProfitSurprise.tdx.txt")
+    export_helper.export("/Users/abing/Downloads/ProfitSurprise.tdx.txt")
