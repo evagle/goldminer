f22355595a4b23f5eb2a339f441c6930d54c4487
abing <binglianghuang@gmail.com>
2019-12-29 18:03:57 +0800

    clear log

--- goldminer/indicators/ROERankProcessor.py
+++ goldminer/indicators/ROERankProcessor.py
@@ -86,9 +86,7 @@ class ROERankProcessor(BaseIndicatorProcessor):
         for i in range(min(3, len(yearModels))):
             yearScore += self.YEAR_WEIGHTS[i] * self.growth2Score(yearModels[i].ROEAVG)
             sumWeight += self.YEAR_WEIGHTS[i]
-            print(yearModels[i].end_date, yearModels[i].ROEAVG, yearScore)
 
-        print(yearScore / sumWeight)
         return yearScore / sumWeight
 
     def process(self, code, **kwargs):
