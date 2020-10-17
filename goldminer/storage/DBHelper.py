0710d236543d0c6901aba5b98ebe8db2eeeb1ce3
abing <binglianghuang@gmail.com>
2020-10-17 11:07:28 +0800

    Database changed to local

--- goldminer/storage/DBHelper.py
+++ goldminer/storage/DBHelper.py
@@ -4,10 +4,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 
 
-
 class DBHelper:
-    __engine = create_engine('mysql+pymysql://pocketpet:3MjdvUuXqxca6cbc@unionfight.citypet.cn:3306/goldminer?charset=utf8',
-                           pool_recycle=60)
+    __engine = create_engine('mysql+pymysql://goldminer:goldiamond@192.168.10.212:3306/goldminer?charset=utf8',
+                             pool_recycle=60)
     # expire_on_commit should be False, otherwise all data are expired and need rerun query after a commit command
     __DBSessionClass = sessionmaker(bind=__engine, autoflush=False, autocommit=False, expire_on_commit=False)
     __session = __DBSessionClass()
@@ -19,4 +18,3 @@ class DBHelper:
     @staticmethod
     def getEngine():
         return DBHelper.__engine
-
