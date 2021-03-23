41821cad6e439c7d62a158ebdfcc766e6f9ce04c
abing <binglianghuang@gmail.com>
2021-03-23 08:17:23 +0800

    log to relative path

--- scripts/goldminer_daily_run.sh
+++ scripts/goldminer_daily_run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-path="/home/abing/goldminer/scripts"
-PYTHON="/usr/local/anaconda3/bin/python"
+HOME="$(pwd)/.."
 
-$PYTHON $path/goldminer_constituents_spiders > /home/abing/goldminer/log/constituents.log 2>&1
-$PYTHON $path/goldminer_spiders > /home/abing/goldminer/log/spiders.log 2>&1
-$PYTHON $path/goldminer_indicator_builder > /home/abing/goldminer/log/evaluations.log 2>&1
-$PYTHON $path/goldminer_funds > /home/abing/goldminer/log/funds.log 2>&1
+path="$HOME/scripts"
+PYTHON="python"
+
+$PYTHON $path/goldminer_constituents_spiders > $HOME/log/constituents.log 2>&1
+$PYTHON $path/goldminer_spiders > $HOME/log/spiders.log 2>&1
+$PYTHON $path/goldminer_indicator_builder > $HOME/log/evaluations.log 2>&1
+$PYTHON $path/goldminer_funds > $HOME/log/funds.log 2>&1
